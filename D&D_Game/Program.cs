09d176a2c270155aa6c6bbd1d77bc7c0ca2d82af
hiraiKatzumi <frostytheking1@gmail.com>
2025-12-02 15:10:44 +0200

    dnd

---
+++ D&D_Game/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DnD_Game
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome to the D&D Game!");
+            
+        }
+    }
+}
